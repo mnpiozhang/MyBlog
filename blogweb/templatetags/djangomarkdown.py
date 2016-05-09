d308e60bc0f204d62e26fe0d96008a1814806e9a
luckylancer <wd636961@sina.com>
2016-05-09 23:52:12 +0800

    add markdown filter

---
+++ blogweb/templatetags/djangomarkdown.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+import markdown
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+@stringfilter
+def djangomarkdown(value):
+    return mark_safe(markdown.markdown(force_unicode(value),
+                                        extras=["code-friendly"]
+                                        )
+                     )
