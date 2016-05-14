47f9afeb65b73111be31eb82fce0d420f5147472
luckylancer <wd636961@sina.com>
2016-05-14 11:36:17 +0800

    更新

--- blogweb/models.py
+++ blogweb/models.py
@@ -4,23 +4,23 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length = 60)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add = True)
-    tag = models.ManyToManyField('TagInfo',blank = True)
+    title = models.CharField(max_length = 60,verbose_name = u'标题')
+    content = models.TextField(verbose_name = u'内容')
+    timestamp = models.DateTimeField(auto_now_add = True,verbose_name = u'时间戳')
+    tag = models.ManyToManyField('TagInfo',blank = True,verbose_name = u'分类标签')
     def __unicode__(self):
         return self.title
     
 class TagInfo(models.Model):
-    tagname = models.CharField(max_length = 20)
+    tagname = models.CharField(max_length = 20,verbose_name = u'标签名称')
     def __unicode__(self):
         return self.tagname
     
     
 class AboutMe(models.Model):
-    authname = models.CharField(max_length = 20)
-    age = models.IntegerField()
-    work = models.CharField(max_length = 50)
-    introduce = models.TextField()
+    authname = models.CharField(max_length = 20,verbose_name = u'作者名称')
+    email = models.EmailField(blank = True,verbose_name = u'邮箱')
+    introduce = models.TextField(blank = True,verbose_name = u'简介')
     def __unicode__(self):
         return self.authname
+    
