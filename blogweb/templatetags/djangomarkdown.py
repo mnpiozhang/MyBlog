136bbc3b62dc28aeedaf881039177fc7be3151e5
luckylancer <wd636961@sina.com>
2016-05-17 01:01:14 +0800

    DD

--- blogweb/templatetags/djangomarkdown.py
+++ blogweb/templatetags/djangomarkdown.py
@@ -12,14 +12,15 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def djangomarkdown(value):
+    '''
     return mark_safe(markdown.markdown(force_unicode(value),
                                         #extensions=["markdown.extensions.codehilite"]
                                          ['codehilite']
                                         )
                      )
     '''
-    return mark_safe(markdown.markdown(value,
-        extensions = ['markdown.extensions.fenced_code', 'markdown.extensions.codehilite'],
-                                    safe_mode=True,
-                                    enable_attributes=False))
-    '''
+    return mark_safe(markdown.markdown(force_unicode(value),
+                                       extensions = ["nl2br","codehilite"],
+                                       safe_mode=True,
+                                       enable_attributes=False))
+    
