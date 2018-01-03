e92aa398d682332d9ff03e5ce4e2ebad112770cb
hzl <cloudhzl@163.com>
2018-01-03 15:28:03 +0800

    更新api返回结果

--- blogweb/api.py
+++ blogweb/api.py
@@ -38,19 +38,26 @@ class jdBooksApi(View):
     最近30天     month
     '''
     def get(self, request):
+        returndata = {"code":200,"errMsg":"","body":[]}
         item = request.GET.get('item')
         category = request.GET.get('category')
         effectivetime = request.GET.get('effectivetime')
         try:
             topnumber = int(request.GET.get('topnumber'))
         except:
-            return HttpResponse('topnumber must be number',status=400)
+            returndata["code"] = 400
+            returndata["errMsg"] = 'topnumber must be number'
+            return HttpResponse(json.dumps(returndata,ensure_ascii=False,indent=2),content_type="application/json")
         if all([item,category,effectivetime,topnumber]):
             if item not in cfg.ITEM.keys() or category not in cfg.CATEGORY.keys() or effectivetime not in cfg.EFFECTIVE_TIME.keys() or topnumber<=0 or topnumber>100:
-                return HttpResponse('request error111',status=400)
+                returndata["code"] = 400
+                returndata["errMsg"] = 'request error111'
+                return HttpResponse(json.dumps(returndata,ensure_ascii=False,indent=2),content_type="application/json")
             else:
                 if effectivetime != 'day' and (item == 'bc' or item == 'nbc'):
-                    return HttpResponse('热评榜只有24小时内的',status=400)
+                    returndata["code"] = 400
+                    returndata["errMsg"] = '热评榜只有24小时内的'
+                    return HttpResponse(json.dumps(returndata,ensure_ascii=False,indent=2),content_type="application/json")
                 else:
                     #a is json, like {"top1": {"url": "//item.jd.com/12236229.html", "name": "妖猫传（沙门空海·大唐鬼宴 全四册经典套装）", "pic": "//img13.360buyimg.com/n3/jfs/t12199/194/878683607/225186/13de2d7c/5a15320dNdfbe411e.jpg"}, "top2": {"url": "//item.jd.com/12239650.html", "name": "余华作品：活着", "pic": "//img14.360buyimg.com/n3/jfs/t10162/279/1390942739/246693/50c56f9d/59e02214N37418280.jpg"}}
                     # i want to trans it 
@@ -60,12 +67,14 @@ class jdBooksApi(View):
                     resultList = []
                     for k,v in a.items():
                         resultList.append({u'rank':k,u'url':v[u'url'],u'name':v[u'name'],u'pic':v[u'pic']})
-                    #print json.dumps(resultList,ensure_ascii=False,indent=2)
-                    resp = HttpResponse(json.dumps(resultList,ensure_ascii=False,indent=2),content_type="application/json")
+                    returndata["body"] = resultList
+                    resp = HttpResponse(json.dumps(returndata,ensure_ascii=False,indent=2),content_type="application/json")
                     #resp["Access-Control-Allow-Headers"] = "content-type"
                     #resp["Access-Control-Allow-Origin"] = "*"
                     #resp["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
                     #resp["Access-Control-Max-Age"] = "1000"
                     return resp
         else:
-            return HttpResponse('request error',status=400)
+            returndata["code"] = 400
+            returndata["errMsg"] = 'request error'
+            return HttpResponse(json.dumps(returndata,ensure_ascii=False,indent=2),content_type="application/json")
