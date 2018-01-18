fc15b18a4de209ad66b19fe33b46f2f674c383cf
mnpiozhang <cloudhzl@163.com>
2018-01-18 11:04:39 +0800

    添加一个showarticlestatus的命令行

---
+++ blogweb/management/commands/showarticlestatus.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+from django.core.management.base import BaseCommand, CommandError
+from blogweb.models import Article,AboutMe,TagInfo,Toys
+
+class Command(BaseCommand):
+    help = 'command show aritcle status'
+    def add_arguments(self,parser):
+        parser.add_argument("aid",nargs='+',type=int)
+    
+    def handle(self,*args,**options):
+        for i in options['aid']:
+            try:
+                article = Article.objects.get(id = i)
+            except Article.DoesNotExist:
+                raise CommandError('article id %s is not exist'%(i))
+            #other logical
+            self.stdout.write("id is %s , title is %s"%(article.id , article.title))
+                
